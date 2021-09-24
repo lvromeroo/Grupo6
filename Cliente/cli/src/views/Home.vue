4ef8d2f358bccd8ad0dec9965a91795ab46b688a
Warspyt <tatianax2442@gmail.com>
2021-09-23 23:54:25 -0500

    Cliente

---
+++ Cliente/cli/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Notas />
+</template>
+
+<script>
+  import Notas from './Notas.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Notas,
+    },
+  }
+</script>
