a1cdba315546567e51643a4058ca65042245859e
lostmundo <sebasmartinez759@gmail.com>
2021-09-26 17:26:25 -0500

    servidor

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const engine = require('ejs-mate');
+
+
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/eniac';
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+
+
+//usando promesas
+mongoose.connect(uri, options).then(
+    () =>{console.log('conectada base de datos')},
+    err => {console.log(err)}
+);
+
+//middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));// en la documentacion estaba true
+
+
+//motor de plantillas
+// app.search('views', path.join(__dirname, 'views'));
+// app.engine("ejs", engine);
+// app.set('view engine', 'ejs');
+
+
+
+//rutas
+app.use('/api', require('./routes/user'));
+
+
+// middleware para vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public'))); // se puede modificar debido a que public no será la unica direccion
+
+//puerto
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), function() {
+    console.log('app listening on por '+ app.get('puerto'));
+});
+
+
+
+
+
+
+
---
+++ backend/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+const saltRaounds = 10;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true}
+});
+
+UserSchema.pre('save', function(next){
+    if(this.isNew || this.ismodified('password')){
+        
+        const document = this;
+        bcrypt.hash(document.password, saltRaounds, (err, hashedPassword) =>{
+            if (err){
+                next(err);
+            }else{
+                document.password = hashedPassword;
+                next();
+            }
+        });
+    }else{
+        next();
+    }
+});
+
+UserSchema.methods.isCorrectPassword = function(password, callback){
+    bcrypt.compare(password, this.password, function(err, same){
+        if(err){
+            callback(err);
+        }else{
+            callback(err, same);
+        }
+    });
+}
+
+//convertir modelo
+const User = mongoose.model('User', UserSchema);
+
+export default User;
