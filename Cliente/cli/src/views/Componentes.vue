3a5666280b9b2a29ebee544bb3d125a3a011439a
Warspyt <tatianax2442@gmail.com>
2021-10-01 00:26:15 -0500

    articulos base de datos

--- Cliente/cli/src/views/Componentes.vue
+++ Cliente/cli/src/views/Componentes.vue
@@ -7,13 +7,13 @@
                 <section class="categorias">
 
                     <br>
-                    <h2 style="color:rgb(34, 1, 95); padding: 0px 15px;" class="category-title">Categorias</h2>
+                    <h2 style="color:rgb(34, 1, 95); padding: 50px 0px 0px 15px;" class="category-title">Categorias</h2>
                     <br>
 
                     <!-- Inicio menu de categorias -->
                     <div class="col-8">
                         <b-button-group vertical style="color:white;">
-                            <b-button variant="primary" >Tarjetas madre</b-button>
+                            <b-button variant="primary">Tarjetas madre</b-button>
                             <b-button variant="primary">Procesadores (CPU)</b-button>
                             <b-button variant="primary">Tarjetas de video(GPU)</b-button>
                             <b-button variant="primary">Memorias RAM</b-button>
@@ -37,7 +37,7 @@
                     <!-- Inicio menu de busqueda de articulos -->
                     
                     <b-navbar type="light" variant="faded">
-                        <b-navbar-nav style="padding: 0px 0px 0px 380px;">
+                        <b-navbar-nav style="padding: 10px 0px 0px 430px;">
                             <!-- Navbar dropdowns -->
                             <b-nav-item-dropdown text="Ordenar por" right>
                                 <b-dropdown-item href="#">Nombre</b-dropdown-item>
@@ -59,28 +59,28 @@
                     <br>
 
                     <!-- Inicio listado de articulos -->
-                    <div class="row">
 
-                        <div class="col-md-4 well"> 
-                            <div class="img-articulo">
-                                <img src="../assets/Mother1.jpg" alt="Tarjeta madre 1" class="imagen-componente" style="width: 250px; height: 187.5px;"/>
-                            </div>
+                    <div style="padding:15px 0px;" class="row" v-for="(item, index) in articulos" :key="index">
+
+                        <div class="col-md-4 well">
+                            <img :src="`${item.imagen}`" :alt="`${item.imagen}`" style="width: 250px; height: 187.5px;"/>
                         </div>
-                        <div class="col-md-4 well" style="padding: 15px;">
-                            <h2>Nombre articulo 1</h2>
+                        <div class="col-md-5 well" style="padding: 15px;">
+                            <h2>{{item.nombreComercial}}</h2>
                             <br>
                 
-                            <h3>Precio: COP$ 0000</h3>
-                            <h3>$ 00.00</h3>
+                            <h3>Precio: COP$ {{item.precioCop}}</h3>
+                            <h3 style="padding: 0px 0px 0px 150px;">$ {{item.preciodolar}}</h3>
                         </div>
-                        <div class="col-md-4 well">
-                            <div class="d-grid gap-2 col-7 mx-auto" style="padding: 20px 0px;">
+                        <div class="col-md-3 well">
+                            <div class="d-grid gap-2 col-10 mx-auto" style="padding: 20px 0px;">
                                 <a href="Articulo_detallado.html" class="btn btn-primary" role="button" style="color:white">Ver detalles</a>
                                 <button class="btn btn-primary" type="button" style="color:white">Agregar al diseño</button>
                                 <button class="btn btn-primary" type="button" style="color:white;">Agregar al carrito</button>
                             </div>
                         </div>
                     </div>
+                    <br>
                     
                 </section>
                 <!-- Fin seccion de vista de articulos --> 
@@ -88,3 +88,45 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    
+
+    data() {
+        return {
+
+            articulos:[],
+        }
+
+    },
+
+    created () {
+
+        this.mostrarArticulos();
+
+    },
+
+    methods: {
+
+        mostrarArticulos() {
+
+            this.axios.get('/articulo')
+            .then(res=>{
+
+                console.log(res.data)
+                this.articulos = res.data;
+
+            })
+            .catch(e=>{
+
+                console.log(e.response);
+
+            })
+
+        },
+
+
+    },
+}
+</script>
