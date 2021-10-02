87de8aa6f376fd52b165838a2ef3cbff3f578bcb
Warspyt <tatianax2442@gmail.com>
2021-10-01 21:17:22 -0500

    .

--- Cliente/cli/src/router/index.js
+++ Cliente/cli/src/router/index.js
@@ -11,14 +11,6 @@ const routes = [
     component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/inicio',
     name: 'Inicio',
     // route level code-splitting
